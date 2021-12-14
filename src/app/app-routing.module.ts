25a0b5e6772dc1aa2ddfaca1b732e47b642e8018
J7 <J7DevWeb@gmail.com>
2021-12-14 11:25:53 +0100

    components added and routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TodoComponent } from './todo/todo.component';
+import { WeatherComponent } from './weather/weather.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'weather', component: WeatherComponent},
+    {path: 'todo', component: TodoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
